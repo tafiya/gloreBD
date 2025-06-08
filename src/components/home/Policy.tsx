bf2b999248a0e409213c40caa18b60c2b4559693
tafiya <tafiapinkey@email.com>
2025-06-09 02:36:59 +0600

    polish the policy section

--- src/components/home/Policy.tsx
+++ src/components/home/Policy.tsx
@@ -1,39 +1,42 @@
-import React from 'react';
-
-import { RefreshCw, BadgeCheck, Headphones } from "lucide-react";
+import { Headset } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+import retu from "../../assets/return.webp";
+import policy from "../../assets/download.png";
 const Policy = () => {
-    return (
-       <div className="bg-pink-100 py-10 px-4">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center">
+  return (
+    <div className="bg-accent py-10 px-4">
+      <div className=" mx-auto grid grid-cols-1 sm:grid-cols-3 gap-10 text-center">
         {/* Policy 1 */}
-        <div className="flex flex-col items-center gap-3">
-          <RefreshCw className="w-12 h-12" />
-          <h3 className="font-bold text-lg sm:text-xl">Easy Exchange Policy</h3>
-          <p className="text-sm sm:text-base">
+        <div className="flex flex-col items-center ">
+          <Image src={policy} className="w-12 h-12 mb-4" alt="return"></Image>
+          <h3 className="font-bold text-base">Easy Exchange Policy</h3>
+          <p className="text-sm font-medium ">
             We offer hassle-free exchange policy
           </p>
         </div>
 
         {/* Policy 2 */}
-        <div className="flex flex-col items-center gap-3">
-          <BadgeCheck className="w-12 h-12" />
-          <h3 className="font-bold text-lg sm:text-xl">3 Days Return Policy</h3>
-          <p className="text-sm sm:text-base">
+        <div className="flex flex-col items-center ">
+          <Image src={retu} className="w-12 h-12 mb-4" alt="return"></Image>
+          {/* <BadgeCheck  /> */}
+          <h3 className="font-bold text-base">3 Days Return Policy</h3>
+          <p className="text-sm font-medium">
             We provide 3 days free return policy
           </p>
         </div>
 
         {/* Policy 3 */}
-        <div className="flex flex-col items-center gap-3">
-          <Headphones className="w-12 h-12" />
-          <h3 className="font-bold text-lg sm:text-xl">Best Customer Support</h3>
-          <p className="text-sm sm:text-base">
+        <div className="flex flex-col items-center">
+          <Headset className="w-12 h-12 mb-4" />
+          <h3 className="font-bold text-base">Best Customer Support</h3>
+          <p className="text-sm font-medium">
             We provide 24/7 customer support
           </p>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Policy;
