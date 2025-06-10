5a5272f1a40a2b58f112577bedbf8e3745106b43
tafiya <tafiapinkey@email.com>
2025-06-11 01:58:51 +0600

    fis the padding issue

--- src/components/home/Collection.tsx
+++ src/components/home/Collection.tsx
@@ -9,17 +9,17 @@ const Collection = async() => {
     return <div className="text-red-600 font-medium">Error: {products.message}</div>;
   }
     return (
-         <div className="bg-pink-50 min-h-screen py-10 px-4">
+         <div className="bg-secondary min-h-screen py-10 ">
       <div className="text-center mb-8">
         <h2 className="text-2xl font-semibold text-gray-700">LATEST COLLECTIONS</h2>
-        <p className="text-pink-500 mt-2">✨ নতুন স্টাইলের ড্রেস উঠেছে ✨</p>
+        <p className="text-primary mt-2">✨ নতুন স্টাইলের ড্রেস উঠেছে ✨</p>
         <p className="text-sm text-gray-500">
           গ্লোরি পণ্যের সাথে থাকুন সর্বশেষ এবং সেরা পার্টি এমব্রয়ডারি গাউন, আপনার পছন্দের{" "}
-          <span className="text-pink-600 font-semibold">GloreBD</span> এর সাথে ❤️
+          <span className="text-primary  font-semibold">GloreBD</span> এর সাথে ❤️
         </p>
       </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 max-w-7xl mx-auto">
+<h2 className=' text-2xl my-4 font-semibold'>Women Clothing</h2>
+      <div className="grid grid-cols-1 px-4 sm:grid-cols-2 md:grid-cols-4 gap-6 mx-auto">
         {products.map((product) => (
           <Card
             key={product._id}
